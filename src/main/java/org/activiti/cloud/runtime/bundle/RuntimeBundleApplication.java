c69984608c8ffe2feb7affdd367ed7e6144a0545
doug4j <doug4j@gmail.com>
2019-01-21 23:22:27 -0500

    Initial import

---
+++ src/main/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplication.java
@@ -0,0 +1,16 @@
+package org.activiti.cloud.runtime.bundle;
+
+import org.activiti.cloud.starter.rb.configuration.ActivitiRuntimeBundle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@ActivitiRuntimeBundle
+public class RuntimeBundleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntimeBundleApplication.class,
+                              args);
+
+    }
+}
---
+++ src/main/resources/processes/README.md
@@ -0,0 +1,3 @@
+# Processes
+Add your bpmn/xml files in this directory to automatically deploy them at Runtime Bundle startup time.
+
